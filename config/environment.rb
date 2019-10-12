9239787d81b3c5402d1616ddf303db0e05a890da
Elana Ryznar <eryzna@gmail.com>
2019-10-12 22:29:07 +0000

    still working out migrations

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,10 @@
-require 'bundler'
-Bundler.require
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/development.sqlite'
-)
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 require_all 'app'
