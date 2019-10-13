cfb5e8766ca193ef876b198e6bb5aa050a7e791e
Elana Ryznar <eryzna@gmail.com>
2019-10-13 17:24:29 +0000

    working on users controller

--- app/controllers/userscontroller.rb
+++ app/controllers/userscontroller.rb
@@ -2,7 +2,16 @@ require './config/environment'
 
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show_user'
+  end
+
   get '/signup' do
-    erb :'users/create_user'
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/activities'
+    end
   end
 end
