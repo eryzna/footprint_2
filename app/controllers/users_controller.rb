8f9a806b2374ee301cdc27046ebf89586708a4b2
Elana Ryznar <eryzna@gmail.com>
2019-10-14 01:00:27 +0000

    create login routes and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,20 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    erb :'/users/login'
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  post '/login' do
+    user= User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/activities'
+    else
+      redirect to '/signup'
+    end
   end
 end
